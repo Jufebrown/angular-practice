ff2e0d52ea8f8a3ece190b45318a017f43651a99
Jufebrown <jufebrown@gmail.com>
2017-01-16 13:29:30 -0600

    added view switching with angular

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var app = angular.module('monkeyButtApp', ['ngRoute'])
 
+app.config(($routeProvider) => {
+  $routeProvider
+  .when('/', {
+    controller: 'MainCtrl',
+    templateUrl: 'partials/main.html'
+  })
+})
+
 app.controller('MainCtrl', function($scope) {
   $scope.potatoes = 'baked!'
 })
