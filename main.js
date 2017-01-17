6f0dd78e3d651796d017b79d83332762bd63b5ff
Jufebrown <jufebrown@gmail.com>
2017-01-17 11:16:57 -0600

    changed to arrow function

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.controller('ListCtrl', function($scope, $http) {
   console.log('here is the list controller')
 
   $http.get(`list.json`)
-  .then(function(val) {
+  .then((val) => {
     console.log('list.json', val.data)
     $scope.list = val.data.list
   })
