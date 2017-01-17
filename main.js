c6a39d32c76daca294de6a4baff71ac90ec436cc
Jufebrown <jufebrown@gmail.com>
2017-01-17 10:18:07 -0600

    added .get to ListCtrl

--- main.js
+++ main.js
@@ -17,6 +17,12 @@ app.controller('MainCtrl', function($scope) {
   $scope.potatoes = 'baked!'
 })
 
-app.controller('listCtrl', function($scope) {
+app.controller('ListCtrl', function($scope, $http) {
   console.log('here is the list controller')
+
+  $http.get(`list.json`)
+  .then(function(val) {
+    console.log('list.json', val.data)
+    $scope.list = val.data.list
+  })
 })
