b5b8d43ad0b17d138d4e1ea47935fe4e641760f4
Jufebrown <jufebrown@gmail.com>
2017-01-18 11:32:42 -0600

    added simple factory interaction to main controller

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,6 +1,10 @@
-app.controller('MainCtrl', function($scope) {
+app.controller('MainCtrl', function($scope, simpleFactory, myFactory) {
   $scope.potatoes = 'baked!'
   $scope.onClickFunction = () => {
-
+  }
+  $scope.mainVar = simpleFactory.getVariable()
+  $scope.setVariable = function(newVariable) {
+    console.log('new variable is', newVariable)
+    simpleFactory.setVariable(newVariable)
   }
 })
