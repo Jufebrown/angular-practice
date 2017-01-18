9bd9bf451f6eb918f9007a30a6f17622bd4271d9
Jufebrown <jufebrown@gmail.com>
2017-01-18 09:27:27 -0600

    deleted console.logs

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,9 +1,6 @@
 app.controller('ListCtrl', function($scope, $http) {
-  console.log('here is the list controller')
-
   $http.get(`list.json`)
   .then((val) => {
-    console.log('list.json', val.data)
     $scope.list = val.data.list
   })
 })
